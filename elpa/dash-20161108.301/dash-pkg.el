c52d4672293593a7844701b1e44142724557b961
Nicolas Mota <nicolas.alves@luizalabs.com>
2016-11-14 14:31:18 -0200

    Add emacs config

---
+++ elpa/dash-20161108.301/dash-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dash" "20161108.301" "A modern list library for Emacs" 'nil :keywords '("lists"))
---
+++ elpa/neon-mode-20160811.216/neon-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "neon-mode" "20160811.216" "Simple major mode for editing neon files" 'nil :keywords '("conf"))
